22f0a7582ee817c27edcce8191a101225d5e9b92
Frank Horrigan <redmmond@gmail.com>
2020-02-14 15:09:26 +1000

    Add project files.

---
+++ blazormobile/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blazormobile
+{
+    public class TodoItem
+    {
+        public string Text { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
